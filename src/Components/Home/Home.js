e11b520e292ce0360bca68b480e0b02a26f3b4c8
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-02-25 13:07:14 +0530

    added m65 v7 prepare for delete

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users,setUsers] = useState([]);
+    useEffect(()=>{
+          fetch('http://localhost:5000/user')
+          .then(res =>res.json())
+          .then(data =>setUsers(data))
+    },[])
+    const handleUserDelete = (id) =>{
+        const proceed = window.confirm('Are you sure you want to delete?')
+        if(proceed){
+            console.log('deleting user with ID',id)
+        }
+    }
     return (
-        <div>
-           {/* <AddUser></AddUser> */}
-           <h1>Welcome to Home</h1>
+        <div className='text-center'>
+           <h1>Users Available:{users.length}</h1>
+           <ul>
+            {
+                users.map(user => <li 
+                key={user._id}>{user.name}::{user.email}
+                <button onClick={()=>handleUserDelete(user._id)}>X</button>
+                </li>)
+            }
+           </ul>
         </div>
     );
 };
