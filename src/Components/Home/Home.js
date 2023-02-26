cb15db1f565575730356c7fd67c6157480793e23
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-02-26 21:55:55 +0530

    m65v8 delete button set up with delete from db and UI

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [users,setUsers] = useState([]);
@@ -7,19 +8,32 @@ const Home = () => {
           .then(res =>res.json())
           .then(data =>setUsers(data))
     },[])
-    const handleUserDelete = (id) =>{
+    const handleUserDelete = id =>{
         const proceed = window.confirm('Are you sure you want to delete?')
         if(proceed){
             console.log('deleting user with ID',id)
+            const url = (`http://localhost:5000/user/${id}`);
+            fetch(url, {
+                method: 'DELETE' 
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                if(data.deletedCount > 0){
+                    console.log('data',data)
+                    const remaining = users.filter(user => user._id !== id);
+                    setUsers(remaining);
+                }
+            })
         }
     }
     return (
         <div className='text-center'>
-           <h1>Users Available:{users.length}</h1>
+           <h1 >Users Available: {users.length}</h1>
            <ul>
             {
                 users.map(user => <li 
                 key={user._id}>{user.name}::{user.email}
+                <Link to={`/update/${user._id}`}><button>Update</button></Link>
                 <button onClick={()=>handleUserDelete(user._id)}>X</button>
                 </li>)
             }
